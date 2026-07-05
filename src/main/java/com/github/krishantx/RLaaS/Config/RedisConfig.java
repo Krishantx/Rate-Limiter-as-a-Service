fa133298d836b600408533d17bb5609c29678a6f
Krishant Basotra <krishantbasotra07@gmail.com>
2026-07-05 21:49:23 +0530

    Version 1

--- src/main/java/com/github/krishantx/RLaaS/Config/RedisConfig.java
+++ src/main/java/com/github/krishantx/RLaaS/Config/RedisConfig.java
@@ -1,10 +1,10 @@
 package com.github.krishantx.RLaaS.Config;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import com.github.krishantx.RLaaS.Model.TokenBucket;
@@ -12,16 +12,15 @@ import com.github.krishantx.RLaaS.Model.TokenBucket;
 @Configuration
 public class RedisConfig {
 
-    @Bean
-    public RedisTemplate<String, TokenBucket> redisTemplate(RedisConnectionFactory connectionFactory) {
-        RedisTemplate<String, TokenBucket> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(connectionFactory);
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(
-            GenericJackson2JsonRedisSerializer.builder().build()
-        );
-        redisTemplate.afterPropertiesSet();
-        return redisTemplate;
-    }
-    
+  @Bean
+  public RedisTemplate<String, TokenBucket> redisTemplate(RedisConnectionFactory connectionFactory) {
+    RedisTemplate<String, TokenBucket> redisTemplate = new RedisTemplate<>();
+    redisTemplate.setConnectionFactory(connectionFactory);
+    redisTemplate.setKeySerializer(new StringRedisSerializer());
+    redisTemplate.setValueSerializer(
+        GenericJackson2JsonRedisSerializer.builder().build());
+    redisTemplate.afterPropertiesSet();
+    return redisTemplate;
+  }
+
 }
